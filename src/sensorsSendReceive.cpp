5cd657f2bdbc42b74d1ef9f454cf0dd692393406
David Morton <davidmorton@xtra.co.nz>
2024-04-03 22:05:58 +1300

    Working commit

--- src/sensorsSendReceive.cpp
+++ src/sensorsSendReceive.cpp
@@ -1,3 +1,18 @@
 #include "sensorsSendReceive.h"
 #include <PID_v1.h>
 
+/*
+Define pin constants
+*/
+const byte inputPotentiometerSignalPin = A15;
+const byte inputPotentiometerSignalPin = A15;
+
+/*
+Define function - Get motor potentiometer voltage reading
+*/
+void getMotorPotentiometerVoltage() {
+  // Voltage range seems to be from 3.25V to 1.70V
+  motorPotentiometerVoltage = analogRead(motorPotentiometerSignalPin) * (5.0 / 1023.0);
+  SERIAL_PORT_MONITOR.print("Motor potentiometer voltage is: ");
+  SERIAL_PORT_MONITOR.println(motorPotentiometerVoltage);
+}
